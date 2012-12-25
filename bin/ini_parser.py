c0b26c62758b9000a3490b64d7018cc334ea0e36
Petr Kolacek <xkolac11@stud.fit.vutbr.cz>
2012-12-25 10:15:00 +0100

    docstrings

--- bin/ini_parser.py
+++ bin/ini_parser.py
@@ -1,4 +1,10 @@
-import ConfigParser
+import ConfigParser #tohle je modul s funkcemi pro praci s ini, cfg, ... soubory
+
+"""
+Nevim, jestli je nutne udelat tridu a vsechny
+metody do ni dat, nebo proste jenom
+metody bez tridy.
+"""
 
 """
 Rozparsuje vstupni soubor
