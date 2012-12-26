ebf73b74ea0c43633f11a8e8b1ac4b77b09df30a
Petr Kolacek <xkolac11@stud.fit.vutbr.cz>
2012-12-26 12:48:27 +0100

    docstring removed

--- bin/ini_parser.py
+++ bin/ini_parser.py
@@ -1,10 +1,5 @@
 import ConfigParser #tohle je modul s funkcemi pro praci s ini, cfg, ... soubory
 
-"""
-Nevim, jestli je nutne udelat tridu a vsechny
-metody do ni dat, nebo proste jenom
-metody bez tridy.
-"""
 
 class parser:
 
